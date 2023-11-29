b3f5634f6e4c281d99a04822614f757cf6ae288b
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2023-11-29 14:27:04 +0100

    Creazione fun. random num & creazione array

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 /*
 Consegna
-L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 */
 
-/*
-Bonus
-Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-*/
+//DIVISIONE DEL PROBLEMA IN SOTTO PROBLEMI:
+// 1 Devo far generare al computer 16 numeri casuali (max una bomba per cella)
+// 2 Al click dell'utente la cella si colora di rosso OVVERO la cella contenente LA BOMBA
+// 3 Al click di una bomba la partita termina
+// 4 Al termine della partita comunichiamo il punteggio (cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba)
 
-//Scomposizione del problema:
-// 1 L'utente clicca sul bottone play per generare la griglia di gioco
-// 2 Ogni cella dovra' avere un numero progressivo da 1 a 100 (for)
-// 3 Al click dell'utente si colora la cella e dovra' uscire in console il numero della cella cliccata
+/*************************************************
+    GLOBAL VARIABLES
+ *************************************************/
+//Selezione del contenitore all'interno del mio index
+const gridElement = document.getElementById("grid");
+//Richiamo il mio array vuoto prima espresso nella funzione e gli dico cose nello specifico
+//const newArrNum = genArrayRandomNum(1, 16, 100);
+// console.log(newArrNum);
+
+
+
+
+
 
 //Si showa la griglia al click
 const btnPlay = document.getElementById('playbtn');
@@ -28,8 +36,6 @@ btnPlay.addEventListener('click',
         grigliaNascosta.style.display = "block";
     }
 );
-
-const gridElement = document.getElementById("grid");
 
 //Ciclo for per avere le mie celle
 for (let i = 1; i <= 100; i++) {
@@ -47,7 +53,12 @@ for (let i = 1; i <= 100; i++) {
     );
 }
 
-//Definisco la funzione
+
+/*************************************************
+    FUNCTIONS
+ *************************************************/
+
+//Definisco la funzione per il tipo di tag ed il nome della classe
 function createMyElement(tagtype, classname) {
 
     const currentElement = document.createElement(tagtype);
@@ -55,3 +66,32 @@ function createMyElement(tagtype, classname) {
 
     return currentElement;
 };
+
+//Definisco la funzione che crea un array, richiamando anche quella di due valori randomici (min, max)
+function genArrayRandomNum(minNum, maxNum, lengthArr) {
+
+    //Creo il mio array vuoto da "popolare" e poi da far ritornare il valore dell'array stesso pero' "completo"
+    let arrayToGen = [];
+
+    //Ciclo che popolera' il mio array
+    while (arrayToGen.length < lengthArr) {
+
+        //richiamo la mia funzione per generare un numero random (min, max)
+        let newNumber = genRandomNum(minNum, maxNum);
+
+        //allora SE il mio numero generato NON e' nell'array:
+        if (!arrayToGen.includes(newNumber)) {
+
+            //ALLORA lo inserisco nell'array
+            arrayToGen.push(newNumber);
+        }
+    }
+
+    return arrayToGen;
+}
+
+//Definisco la funzione per generare un numero random in un range di (min, max) 
+//(in questo caso funzione utile da richiamare per generare il mio array con min max randomici)
+function genRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
